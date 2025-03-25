7c57b945b7d52602ada3e06629c59254c853970e
Андрей Левченков <lev_a_a@mail.ru>
2025-03-25 20:45:59 +0200

    Prefixes

--- BackEndDebian/Program.cs
+++ BackEndDebian/Program.cs
@@ -4,8 +4,8 @@ using BackEndDebian.Controller;
 
 HttpClient httpClient = new HttpClient();
 HttpListener server = new HttpListener();
-//server.Prefixes.Add("http://193.104.57.148:8080/connection/");
-server.Prefixes.Add("http://127.0.0.1:8888/connection/");
+server.Prefixes.Add("http://193.104.57.148:8080/connection/");
+//server.Prefixes.Add("http://127.0.0.1:8888/connection/");
 server.Start();
 while (true)
 {
