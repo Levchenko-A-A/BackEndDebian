4705eaec61076fd10288a3e7e6c52ebac58b87d2
Levchenko-A-A <163921727+Levchenko-A-A@users.noreply.github.com>
2025-03-28 17:04:07 +0200

    IntentaryNamber

---
+++ BackEndDebian/Controller/InventorynumberController.cs
@@ -0,0 +1,35 @@
+﻿using BackEndDebian.Controller.StaticServic;
+using BackEndDebian.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BackEndDebian.Controller
+{
+    internal class InventorynumberController
+    {
+        public async static void getInventorynumber(HttpListenerContext context)
+        {
+            string json = await DataHendler.GetDataAsJson<Person>(db => db.Persons);
+            await DataHendler.SendJsonResponse(context, json);
+        }
+        public async static void getInventorynumberId(string json, HttpListenerContext context)
+        {
+            int id = JsonSerializer.Deserialize<int>(json);
+            using (DbinventoryContext db = new DbinventoryContext())
+            {
+                Inventorynumber? inventorynumber = await db.Inventorynumbers.FirstOrDefaultAsync(i => i.Deviceid == id);
+                if (inventorynumber != null)
+                {
+                    string jsonInvent = JsonSerializer.Serialize<Inventorynumber>(inventorynumber);
+                    await DataHendler.SendJsonResponse(context, jsonInvent);
+                }
+            }
+        }
+    }
+}
--- BackEndDebian/Model/Inventorynumber.cs
+++ BackEndDebian/Model/Inventorynumber.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace BackEndDebian.Model;
 
 public partial class Inventorynumber
 {
+    [JsonPropertyName("inventorynumberid")]
     public int Inventorynumberid { get; set; }
-
-    public int? Deviceid { get; set; }
-
+    [JsonPropertyName("deviceid")]
+    public int Deviceid { get; set; }
+    [JsonPropertyName("number")]
     public string Number { get; set; } = null!;
-
+    [JsonPropertyName("createdat")]
     public DateTime? Createdat { get; set; }
-
+    [JsonIgnore]
     public virtual Device? Device { get; set; }
 }
